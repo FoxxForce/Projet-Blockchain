84bb72e031c02b831b36a787643c8a053304429e
SOUMARE cheikou <cheikou.s99@gmail.com>
2023-03-12 22:47:56 +0100

    amélioration de la structure des noeuds [à tester]

--- client1.py
+++ client1.py
@@ -5,11 +5,8 @@ from node import *
 if __name__ == "__main__":  
     b = Block(None, "Hello")
     chain = Blockchain("okok")
-    # b.proof_of_work()
-    # print(b.hash())
     chain.add_block(b)
-    # print(chain.is_valid_blockchain())
-    print(chain.blockchain_to_string())
-    node = Node(chain)
-    node.broadcast_blockchain()
-    #chain2 = node.receive_blockchain()
+    node = Node("225.1.2.4")
+    node.blockchain = chain
+    node.run_node()
+
