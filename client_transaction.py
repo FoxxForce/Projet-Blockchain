e472507e13488c538e2b4000c3aa9a2a0f59560c
SOUMARE cheikou <cheikou.s99@gmail.com>
2023-04-17 12:58:36 +0200

    madification client_transction

--- client_transaction.py
+++ client_transaction.py
@@ -4,6 +4,6 @@ import threading
 
 if __name__ == "__main__":
     wallet_sender = Wallet()
-    public_key_receiver = input("Enter the public key of the receiver: ")
+    public_key_receiver = "-----BEGIN RSA PUBLIC KEY-----\nMEgCQQDEIsEdTe+hgpNszqsT8+xyOqMRAIQxeSyTh1HV0eKm0L+PPz0NJH6qBgG9\nRI6UF4wpO4mtutwySruFaFvns8HVAgMBAAE=\n-----END RSA PUBLIC KEY-----\n"
     amount = int(input("Enter the amount of the transaction: "))
     Node.send_transaction("225.1.2.5", wallet_sender, public_key_receiver, amount)
